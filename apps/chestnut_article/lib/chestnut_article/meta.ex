70be79c7a8a6fe1fa7b069c28c92ce82586d2244
RoastChestnut <chestnut.roasted@outlook.com>
2024-07-15 21:04:33 +0800

    [Content][Meta] 添加了类型

--- apps/chestnut_article/lib/chestnut_article/meta.ex
+++ apps/chestnut_article/lib/chestnut_article/meta.ex
@@ -1,3 +1,15 @@
 defmodule ChestnutArticle.Meta do
-  defstruct [:id, :title, :create_at, :update_at]
+  @type t :: %__MODULE__{
+    id: String.t(),
+    title: String.t(),
+    create_at: DateTime.t(),
+    update_at: DateTime.t(),
+    tags: list(String.t()),
+    extra: map()
+  }
+  defstruct [:id, :title, :create_at, :update_at, :tags, :extra]
+
+  def validid?(id) do
+    Regex.match?(~r/^[a-zA-Z0-9-]+$/, id)
+  end
 end
